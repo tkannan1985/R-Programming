bb9f4908812a3936385f29b846d3c2c7bd04d377
Kannan Thiruvengadam <tkannan1985@gmail.com>
2014-12-21 14:10:09 +0530

    R Programming
    Assignment2

---
+++ cachematrix.R
@@ -0,0 +1,66 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  y<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
+}
+
+##############Second Type################################
+
+makeVector <- function(x = numeric())
+{
+  m <- NULL
+  y <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean)
+    m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
